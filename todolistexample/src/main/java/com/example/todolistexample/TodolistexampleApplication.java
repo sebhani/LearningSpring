6c0f0f4fcae8299ac2d066575ddfc43450eb0aa4
sebhani <hani-111-222@hotmail.com>
2020-04-21 07:15:58 -0400

    Initial spring project setup

---
+++ todolistexample/src/main/java/com/example/todolistexample/TodolistexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolistexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistexampleApplication.class, args);
+	}
+
+}
---
+++ todolistexample/src/test/java/com/example/todolistexample/TodolistexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolistexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
