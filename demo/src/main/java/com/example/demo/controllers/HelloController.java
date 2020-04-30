8ceb07135781a7958611b1bd59fbf1e52fa7e9e1
sebhani <hani-111-222@hotmail.com>
2020-04-30 10:16:15 -0400

    Pass parameters: 3- Url segment

--- demo/src/main/java/com/example/demo/controllers/HelloController.java
+++ demo/src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 //@RequestMapping(value = "home")   //will make the path /home/...
 public class HelloController {
 
+    //This method is for retrieving data within http requests
     @RequestMapping(value="")
     @ResponseBody
     public String index(HttpServletRequest request){ // To retrieve data from http request, user HttpServletRequest --> .getParameter("name")
@@ -22,6 +24,8 @@ public class HelloController {
         return "Hello "+name;
     }
 
+    //Following two methods are to handle POST requests
+    //step #1: handling getting requests by displaying the form to get user input
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     @ResponseBody
     public String helloForm(){
@@ -33,11 +37,19 @@ public class HelloController {
         return html;
     }
 
+    //step #2: retrieving posted data entered by users
     @RequestMapping(value = "hello", method = RequestMethod.POST)
     @ResponseBody
     public String helloPost(HttpServletRequest request){
         String name = request.getParameter("name");
 
+        return "Hello "+name;
+    }
+
+    //This method is to handle segment data passing
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name){//parameter should match the one within curly braces (i.e. both 'name')
         return "Hello "+name;
     }
 
