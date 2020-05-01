d0313ae85a280b975083416245ac5953d2dc8407
sebhani <hani-111-222@hotmail.com>
2020-05-01 03:31:30 -0400

    Pass data from controller handler to Thymeleaf template

--- demo/src/main/java/com/example/demo/controllers/CheeseController.java
+++ demo/src/main/java/com/example/demo/controllers/CheeseController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +10,8 @@ public class CheeseController {
 
     // Request path: /cheese
     @RequestMapping(value = "")
-    public String index(){
+    public String index(Model model){//Model is to pass data from controller handler to view
+        model.addAttribute("title","My Cheeses");
         return "cheese/index";
     }
 }
