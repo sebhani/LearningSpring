3cefc20e44f052a2d43fc7f24fede288b5e354a7
sebhani <hani-111-222@hotmail.com>
2020-05-01 05:03:43 -0400

    Create a dynamic unordered list of cheeses and handle empty lists

--- demo/src/main/java/com/example/demo/controllers/CheeseController.java
+++ demo/src/main/java/com/example/demo/controllers/CheeseController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping(value = "cheese")
 public class CheeseController {
@@ -11,6 +13,13 @@ public class CheeseController {
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model){//Model is to pass data from controller handler to view
+
+        ArrayList<String> cheeses = new ArrayList<String>();
+//        cheeses.add("cheddar");
+//        cheeses.add("parmesan");
+//        cheeses.add("munster");
+
+        //model.addAttribute("cheeses",cheeses);
         model.addAttribute("title","My Cheeses");
         return "cheese/index";
     }
