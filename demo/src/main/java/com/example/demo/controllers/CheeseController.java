0b818c2786daff6a572120bb5abd6eeb55e8185a
sebhani <hani-111-222@hotmail.com>
2020-05-03 22:33:11 -0400

    Update controller and view to add remove functionality

--- demo/src/main/java/com/example/demo/controllers/CheeseController.java
+++ demo/src/main/java/com/example/demo/controllers/CheeseController.java
@@ -1,20 +1,17 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 @Controller
 @RequestMapping(value = "cheese")
 public class CheeseController {
-
-    static HashMap<String, String> cheeses = new HashMap<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
@@ -39,9 +36,23 @@ public class CheeseController {
      */
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
-        cheeses.put(cheeseName, cheeseDescription);
+        Cheese newCheese = new Cheese(cheeseName,cheeseDescription);
+        cheeses.add(newCheese);
 
         // Redirect to /cheese. This works because both are within the same controller
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveForm(Model model){
+        model.addAttribute("title","Remove Cheese");
+        model.addAttribute("cheeses",cheeses);
+
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String RemoveCheeseForm(){
         return "redirect:";
     }
 }
