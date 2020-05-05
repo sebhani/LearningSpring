706d59dceea00a7bf6e054f843eada484aa42013
sebhani <hani-111-222@hotmail.com>
2020-05-05 02:57:45 -0400

    Add remove functionality and use @ModelAttribute annotation for requesting data

--- demo/src/main/java/com/example/demo/controllers/CheeseController.java
+++ demo/src/main/java/com/example/demo/controllers/CheeseController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Cheese;
+import com.example.demo.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +19,7 @@ public class CheeseController {
     @RequestMapping(value = "")
     public String index(Model model){//Model is to pass data from controller handler to view
 
-        model.addAttribute("cheeses",cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title","My Cheeses");
         return "cheese/index";
     }
@@ -35,9 +37,14 @@ public class CheeseController {
      * Add cheese form handling
      */
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
-        Cheese newCheese = new Cheese(cheeseName,cheeseDescription);
-        cheeses.add(newCheese);
+    public String processAddCheeseForm(@ModelAttribute Cheese newCheese){
+        //Cheese newCheese = new Cheese(cheeseName,cheeseDescription); //used with @RequestParam annotation
+        /*
+         * Cheese newCheese = new Cheese();
+         * newCheese.setName(Request.getParameter("name"))
+         * newCheese.setDescription(Request.getParameter("description"))
+         */
+        CheeseData.add(newCheese);
 
         // Redirect to /cheese. This works because both are within the same controller
         return "redirect:";
@@ -46,13 +53,17 @@ public class CheeseController {
     @RequestMapping(value = "remove", method = RequestMethod.GET)
     public String displayRemoveForm(Model model){
         model.addAttribute("title","Remove Cheese");
-        model.addAttribute("cheeses",cheeses);
+        model.addAttribute("cheeses",CheeseData.getAll());
 
         return "cheese/remove";
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.POST)
-    public String RemoveCheeseForm(){
+    public String RemoveCheeseForm(@RequestParam int[] cheeseIds){
+
+        for(int cheeseId: cheeseIds){
+            CheeseData.remove(cheeseId);
+        }
         return "redirect:";
     }
 }
---
+++ demo/src/main/java/com/example/demo/models/CheeseData.java
@@ -0,0 +1,36 @@
+package com.example.demo.models;
+
+import java.util.ArrayList;
+
+public class CheeseData {
+
+    static ArrayList<Cheese> cheeses = new ArrayList<>();
+
+    // getAll
+    public static ArrayList<Cheese> getAll(){
+        return cheeses;
+    }
+
+    // add
+    public static void add(Cheese newCheese){
+        cheeses.add(newCheese);
+    }
+
+    // remove
+    public static void remove(int id){
+        Cheese cheeseToRemove = getById(id);
+        cheeses.remove(cheeseToRemove);
+    }
+
+    // getById
+    public static Cheese getById(int id){
+        Cheese newCheese = null;
+
+        for(Cheese candidateCheese : cheeses){
+            if(candidateCheese.getCheeseId() == id)
+                newCheese = candidateCheese;
+        }
+
+        return newCheese;
+    }
+}
