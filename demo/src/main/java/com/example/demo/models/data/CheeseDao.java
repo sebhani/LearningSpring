3ad72e150878fe1df3119f8a1b8075b4680506d4
sebhani <hani-111-222@hotmail.com>
2020-07-04 16:27:52 -0400

    Add the data access object which is used to provide the CRUD operations to our application

---
+++ demo/src/main/java/com/example/demo/models/data/CheeseDao.java
@@ -0,0 +1,13 @@
+package com.example.demo.models.data;
+
+import com.example.demo.models.Cheese;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface CheeseDao extends CrudRepository<Cheese, Integer> {
+
+}
