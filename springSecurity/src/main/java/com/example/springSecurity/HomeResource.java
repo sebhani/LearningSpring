1b050137451ca656d1cd3a8664b866989f20e21c
sebhani <hani-111-222@hotmail.com>
2020-07-05 09:35:57 -0400

    configure authorization by overriding configure(HttpSecurity http) method

--- springSecurity/src/main/java/com/example/springSecurity/HomeResource.java
+++ springSecurity/src/main/java/com/example/springSecurity/HomeResource.java
@@ -10,4 +10,14 @@ public class HomeResource {
     public String home(){
         return "<h1>Welcome</h1>";
     }
+
+    @RequestMapping(value = "/admin")
+    public String admin(){
+        return "<h1>Welcome admin</h1>";
+    }
+
+    @RequestMapping(value = "/user")
+    public String user(){
+        return "<h1>Welcome user</h1>";
+    }
 }
--- springSecurity/src/main/java/com/example/springSecurity/SecurityConfiguration.java
+++ springSecurity/src/main/java/com/example/springSecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.springSecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -9,6 +10,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity //tells Spring Security that this security is web-based and not application/method security
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    /*
+    Authentication configuration
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //set your configuration on the auth object
@@ -27,5 +31,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    /*
+    Authorization configuration
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                //You specify accessibility from the most to the least restrictive
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
