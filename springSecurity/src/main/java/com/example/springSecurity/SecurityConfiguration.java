bc7da4c8bf6a91e81feabf0e974ec3a57417945f
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-07-08 21:43:49 -0400

    Implement UserDetailsService and UserDetails for a hard coded user. This can be connected to any data source (e.g. MySQL database) to retrieve user information

--- springSecurity/src/main/java/com/example/springSecurity/SecurityConfiguration.java
+++ springSecurity/src/main/java/com/example/springSecurity/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -54,13 +55,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    /*
     //our embedded database --> H2
     @Autowired
     DataSource dataSource;
 
-    /*
+
      * JDBC authentication
-     */
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
@@ -101,6 +103,37 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .and().formLogin();
     }
+     */
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    /*
+    // JPA authentication and MySQL
+     */
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    // Authentication configuration
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);//This method enables you to pass an instance of a userDetailsService
+    }
+
+    //2- provide an encoder
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    // Authorization configuration
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                //You specify accessibility from the most to the least restrictive
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
 }
